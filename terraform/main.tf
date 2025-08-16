d4c46079a0856ae1b5aebd92ea478f18095bbe73
Tom Boone <tbc@tomboone.com>
2025-08-16 13:31:47 -0400

    remove mysql from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.1"
     }
-    mysql = {
-      source  = "petoju/mysql"
-      version = "~> 3.0"
-    }
   }
 }
 
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-}
-
-provider "mysql" {
-  endpoint = "${var.mysql_server_name}.mysql.database.azure.com:3306"
-  username = var.mysql_admin_username
-  password = var.mysql_admin_password
 }
