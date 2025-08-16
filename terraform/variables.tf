d6aa39e67ac4086bb3bf28fa5e635e439dc90b02
Tom Boone <tbc@tomboone.com>
2025-08-16 13:36:13 -0400

    remove mysql from variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,20 +21,6 @@ variable "location" {
   type = string
 }
 
-variable "mysql_server_name" {
-  type = string
-}
-
-variable "mysql_admin_username" {
-  type      = string
-  sensitive = true
-}
-
-variable "mysql_admin_password" {
-  type      = string
-  sensitive = true
-}
-
 variable "function_app_name" {
   type = string
 }
