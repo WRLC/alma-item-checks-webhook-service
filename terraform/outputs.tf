e09fdcdfba624d6ce92c87576af9485f61932bcb
Tom Boone <tbc@tomboone.com>
2025-09-19 16:23:15 -0400

    update terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,11 @@ output "function_app_name" {
   value       = azurerm_linux_function_app.function_app.name
 }
 
+output "function_app_resource_group_name" {
+  description = "The resource group of the function app."
+  value       = azurerm_linux_function_app.function_app.resource_group_name
+}
+
 output "python_version" {
   description = "The Python version installed in the function app."
   value       = azurerm_linux_function_app.function_app.site_config[0].application_stack[0].python_version
