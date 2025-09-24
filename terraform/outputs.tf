5af468f943f0e03491386d58c2b278e78dd6f87a
Tom Boone <tbc@tomboone.com>
2025-09-24 14:16:16 -0400

    refactor workflows and updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "python_version" {
   description = "The Python version installed in the function app."
   value       = azurerm_linux_function_app.function_app.site_config[0].application_stack[0].python_version
 }
+
+output "stage_slot_name" {
+  description = "The name of the app's staging deployment slot"
+  value       = azurerm_linux_function_app_slot.staging_slot.name
+}
